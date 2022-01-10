61b4161bc09b5b47f7130be375223a4d6a1d56cc
surekhakallutla <surekhakallutla@gmail.com>
2022-01-10 10:41:39 +0530

    Calculating the distance between Two Co-ordinates (x1, y1) and (x2, y2)

---
+++ LineComparision/src/com/bridgelabz/demo/LineComparisionUC1.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.demo;
+
+import java.util.Scanner;
+
+public class LineComparisionUC1 {
+	
+	public static void main(String[] args)	
+	{
+		System.out.println("Welcome to line Comparision Computation Program");
+				
+		Scanner scn = new Scanner(System.in);
+	
+		System.out.println("Enter (x1, y1) and (x2, y2) co-ordinates of line1");
+		int x1 = scn.nextInt();
+		int y1 = scn.nextInt();
+		int x2 = scn.nextInt();
+		int y2 = scn.nextInt();
+		
+		double line = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));	 	    
+        System.out.println("distance between Line Co-ordinates"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+line);
+        
+	}
+
+}
